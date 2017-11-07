644cab978e63fa4b6dbd897aba6840153ba71d7d
Arsenal <alex962305@list.ru>
2017-11-07 12:52:46 +0300

    cal

---
+++ app/src/main/java/com/example/arsenal/cal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.arsenal.cal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//[p[p[p
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
